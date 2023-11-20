89549fca83cb334c5e4cffad2cf42015538783b4
VincenzoCapaldo <enzo0@LAPTOP-1OJ0HD5N>
2023-11-20 18:02:08 +0100

    Aggiunte finestra1 e finestra3

--- SoftwareEngineeringGruppo05/src/softwareengineeringgruppo05/SoftwareEngineeringGruppo05.java
+++ SoftwareEngineeringGruppo05/src/softwareengineeringgruppo05/SoftwareEngineeringGruppo05.java
@@ -18,7 +18,7 @@ public class SoftwareEngineeringGruppo05 extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
