43582cbdd472987f87a9142162769506a01e5769
Luca <Luca@LAPTOP-HKAG0LM2>
2023-11-20 18:36:42 +0100

    Trigger interface implemented

---
+++ SoftwareEngineeringGruppo05/src/triggers/Trigger.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package triggers;
+
+/**
+ *
+ * @author Luca
+ */
+public interface Trigger {
+    public boolean verify();
+}
